856a54e113baa7afd2826a2b36fe65872921e549
Okan Çelebi <okancelebi06@gmail.com>
2023-01-31 21:02:29 +0300

    Add files via upload

---
+++ csharp mod alma operatoru/csharp mod alma operatoru/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_mod_alma_operatoru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mod alma operatörü : % ( 2 sayının bölümünden kalanı verir)
+            int sayi1 = 17;
+            int sayi2 = 3;
+            Console.WriteLine(sayi1 % sayi2);
+            Console.ReadLine();
+        }
+    }
+}
